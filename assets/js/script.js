15e4974d8b56bc75eeff0667ed76947f798f36d2
thisisgene <emdo2000@gmail.com>
2017-11-08 17:07:26 +0100

    Initial commit.

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+function selectionActive(keyword) {
+
+  $('.about').removeClass('about');
+  $('.reference').removeClass('reference');
+  $('.contact').removeClass('contact');
+  $("#header").addClass('selection-active').addClass(keyword);
+
+  var url = "/assets/view/" + keyword + ".html";
+
+  $('.main-wrapper').removeClass('visible');
+  $('.active').removeClass('active');
+  $('#'+ keyword).addClass('active');
+
+  $.get(url, function(data) {
+    $('.main-wrapper').html(data).addClass('visible');
+  });
+
+}
